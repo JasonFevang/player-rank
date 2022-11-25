95d1dd41ee00054660b0ad9124a97f4811b04ff3
Jason Fevang <jasonfevang@gmail.com>
2022-11-24 20:23:58 -0800

    Refactor into functions

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ const NUM_PLAYERS: usize = 35;
 const NOISE_RANGE: f64 = 0.2;
 const CHANCE_SKIP_ROW: f64 = 0.;
 
-fn main() {
-    // Generate a series of players with random rating in range [0,1)
+// Generate a vector of randomized rankings to seed the matrix with instead of asking questions
+// Results are normalized to the last entry, so the last entry will always be 1
+fn gen_seed_rankings() -> Vec<f64>{
     let mut seed_rankings: Vec<f64> = Vec::new();
     for _ in 0..NUM_PLAYERS{
         seed_rankings.push(rand::random::<f64>());
@@ -18,9 +19,11 @@ fn main() {
     for i in 0..NUM_PLAYERS{
         seed_rankings[i] = seed_rankings[i] / seed_rankings[NUM_PLAYERS - 1];
     }
+    seed_rankings
+}
 
-    println!("{:.3?}", seed_rankings);
-
+// Given a seed vector of rankings of players, generate a test linear system
+fn gen_lin_sys_from_seed_rankings(seed_rankings: &Vec<f64>) -> (Array2<f64>, Array1<f64>){
     // Generate A matrix
     let mut a: Array2<f64> = arr2(&[[]]);
     // First row
@@ -51,6 +54,11 @@ fn main() {
     b[0] = seed_rankings[NUM_PLAYERS - 1];
     let b: Array1<f64> = arr1(&b);
 
+    (a, b)
+}
+
+// Solve a linear system using a least squares regression
+fn least_squares_regression(a: Array2<f64>, b: Array1<f64>) -> Vec<f64>{
     // Perform linear algebra to do least squares regression
     let ata = a.t().dot(&a);
     let atb = a.t().dot(&b);
@@ -60,30 +68,36 @@ fn main() {
     for i in 0..NUM_PLAYERS{
         sol[i] = sol[i] / sol[NUM_PLAYERS - 1];
     }
+    sol
+}
 
-    println!("Solution: {:.3?}", sol);
-
+// Given the seed ranks and the computed result ranks, compute a vector or %error on the computation
+fn compute_err_from_seed(seed_rankings: &Vec<f64>, sol: &Vec<f64>) -> Vec<f64>{
     let mut err: Vec<f64> = Vec::new();
     for i in 0..NUM_PLAYERS{
         err.push(((seed_rankings[i] - sol[i])/seed_rankings[i]).abs());
     }
+    err
+}
 
-    // println!("Errors: {:.3?}", err);
-    // Compute min, max and mean errors on all the players
-    let mut min_err = err[0];
+// Compute and log max and mean errors on all the players
+fn log_err_stats(err: &Vec<f64>){
     let mut max_err = err[0];
     let mut sum = 0.;
-    for er in &err{
+    for er in err{
         sum += er;
-        if *er < min_err {
-            min_err = *er;
-        }
         if *er > max_err {
             max_err = *er;
         }
     }
     let mean = sum / err.len() as f64;
-    // let min_err = err.into_iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    // let max_err = err.into_iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-    println!("Min: {:.3} Max: {:.3} Mean: {:.3}", min_err, max_err, mean);
+    println!("Max: {:.3} Mean: {:.3}", max_err, mean);
+}
+
+fn main() {
+    let seed_rankings = gen_seed_rankings();
+    let (a, b) = gen_lin_sys_from_seed_rankings(&seed_rankings);
+    let sol = least_squares_regression(a, b);
+    let err = compute_err_from_seed(&seed_rankings, &sol);
+    log_err_stats(&err);
 }
