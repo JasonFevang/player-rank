cd20ef0dd7546957aa45ef0071ec5e529143bdbf
Jason Fevang <jasonfevang@gmail.com>
2022-11-24 19:19:27 -0800

    Support >16 players

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use ndarray_linalg::*;
 
 use rand::Rng;
 
-const NUM_PLAYERS: usize = 4;
-const NOISE_RANGE: f64 = 0.1;
+const NUM_PLAYERS: usize = 35;
+const NOISE_RANGE: f64 = 0.05;
 
 fn main() {
     // Generate a series of players with random rating in range [0,1)
@@ -15,9 +15,12 @@ fn main() {
     println!("{:?}", seed_rankings);
 
     // Generate A matrix
-    let mut first_row: [f64; NUM_PLAYERS] = [0.; NUM_PLAYERS];
-    first_row[NUM_PLAYERS - 1] = 1.;
-    let mut a: Array2<f64> = arr2(&[first_row]);
+    let mut a: Array2<f64> = arr2(&[[]]);
+    for _ in 0..NUM_PLAYERS{
+        a.push_column(ArrayView::from(&[0.])).unwrap();
+    }
+    a[[0, NUM_PLAYERS - 1]] = 1.;
+
     for p1 in 0..NUM_PLAYERS{
         for p2 in (p1 + 1)..NUM_PLAYERS{
             let mut next_row: [f64; NUM_PLAYERS] = [0.; NUM_PLAYERS];
@@ -28,18 +31,16 @@ fn main() {
             a.push_row(ArrayView::from(&next_row)).unwrap();
         }
     }
-    println!("seed_mat: {}", a);
 
     // Generate b vector
     let mut b: Vec<f64> = vec![0.; a.dim().0];
     b[0] = seed_rankings[NUM_PLAYERS - 1];
     let b: Array1<f64> = arr1(&b);
-    println!("b_vec: {}", b);
 
     // Perform linear algebra to do least squares regression
     let ata = a.t().dot(&a);
     let atb = a.t().dot(&b);
-    let _x = ata.solve(&atb).unwrap();
+    let _x = ata.solve(&atb).unwrap().to_vec();
 
-    println!("Solution: {}", _x);
+    println!("Solution: {:?}", _x);
 }
